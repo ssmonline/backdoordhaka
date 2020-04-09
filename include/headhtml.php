719fb3c5c7bf2e7f3e497cdf1662762685851314
Backdoor Dhaka <63362334+ssmonline@users.noreply.github.com>
2020-04-10 00:25:50 +0600

    Update headhtml.php

--- include/headhtml.php
+++ include/headhtml.php
@@ -22,7 +22,7 @@ error_reporting(0);
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>MHH-ONLINE || <?= $hotspotname; ?></title>
+		<title>SSM Communication Ltd || <?= $hotspotname; ?></title>
 		<meta charset="utf-8">
 		<meta http-equiv="cache-control" content="private" />
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -47,4 +47,4 @@ error_reporting(0);
 	<body>
 		<div class="wrapper">
 
-			
+		
