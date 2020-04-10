70ea54c5d830a578f2c3696faca6ddfa51d48130
Backdoor Dhaka <63362334+ssmonline@users.noreply.github.com>
2020-04-10 11:23:01 +0600

    Update version.php

--- include/version.php
+++ include/version.php
@@ -2,6 +2,6 @@
 if (!isset($_SESSION["ssmonline"])) {
     header("Location:../admin.php?id=login");
   } else {
-        $_SESSION["v"] = "3.18 09-01-2020 © MHH Computer IT Technology LLC";
+        $_SESSION["v"] = "3.18 © SSM Communication Ltd";
     
     }
